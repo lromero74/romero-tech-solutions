65e8324414863294ba3beaad57f045e2f35da4f3
lromero74 <lromero@romerotechsolutions.com>
2025-10-21 08:10:43 -0400

    chore: Bump version to 1.101.86

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'romero-tech-v1.101.85';
-const STATIC_CACHE_NAME = 'romero-tech-static-v1.101.85';
-const DYNAMIC_CACHE_NAME = 'romero-tech-dynamic-v1.101.85';
+const CACHE_NAME = 'romero-tech-v1.101.86';
+const STATIC_CACHE_NAME = 'romero-tech-static-v1.101.86';
+const DYNAMIC_CACHE_NAME = 'romero-tech-dynamic-v1.101.86';
 
 // Resources to cache immediately
 const STATIC_ASSETS = [
